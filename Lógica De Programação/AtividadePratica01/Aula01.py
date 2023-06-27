e357ea390d43c55b191e2e0bafd9a11604344b8d
lucas <lcs_demais@hotmail.com>
2023-06-27 18:10:10 -0300

    calc final

--- Lógica De Programação/AtividadePratica01/Aula01.py
+++ Lógica De Programação/AtividadePratica01/Aula01.py
@@ -1,10 +1,13 @@
 import math
+
+## classe calculadora
 class Calculadora:
     def __init__(self,numero1,numero2,op):
         self.numero1 = numero1
         self.numero2 = numero2
         self.op = op
     
+    ## Métodos das operações
     def soma(self):
         return f'A soma de {self.numero1} e {self.numero2} foi : {self.numero1 + self.numero2}'
     
@@ -26,6 +29,9 @@ class Calculadora:
     def raiz_soma_dois(self):
         return f'A raiz da soma  de ({self.numero1} e {self.numero2}) foi : {math.sqrt(self.numero1 + self.numero2)}'
     
+    
+    
+    # função que seleciona as operações
     def define_resultado(self):
         if self.op == '+':
             return self.soma()
@@ -41,8 +47,11 @@ class Calculadora:
             return self.resto()
         elif self.op == 'sqrt':
             return self.raiz_soma_dois()
+
+# função menu do programa onde tudo será executado
 def menu():
     while(True):
+        # mennu principal
         op = input('Escolha uma das opções: \n'
            '( + )Soma\n'
            '( - ) Subtração\n'
@@ -52,19 +61,23 @@ def menu():
            '( % )Resto\n'
            '( sqrt )Raiz\n'+
            'Digite a operação desejada: ')
+        # validação se as entrada está correta,aceitando apenas os operadores
         if not op in '+ - / * ** sqrt %':
             print('Valor não idêntificado')
             continue
         else:
+            # validação com try para aceitar apenas números inteiros, volta para o menu principal
             try:
                 n1 = int(input('Digite o penultimo digito do seu RU: '))
                 n2 = int(input('Digite o último digito do seu RU: '))
             except ValueError:
                 print('Apenas número inteiros são válidos')
                 continue
+            # instância do objeto
             calc = Calculadora(n1,n2,op)
             print(calc.define_resultado())
             continua = input('Digite qualque coisa para continuar ou digite "sair" para sair: ')
+            # menu de seleção para conntinuar ou não !!!
             if continua.lower() == 'sair':
                 break
             else:
