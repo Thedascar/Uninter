2002cf232c0a6d5fa4d80f92aba606f9f0098a61
lucas <lcs_demais@hotmail.com>
2023-06-27 15:13:36 -0300

    passos finais da calc

--- Lógica De Programação/AtividadePratica01/Aula01.py
+++ Lógica De Programação/AtividadePratica01/Aula01.py
@@ -6,37 +6,39 @@ class Calculadora:
     
     
     def soma(self):
-        return f'A soma foi de : {self.numero1 + self.numero2}'
+        return f'A soma de {self.numero1} e {self.numero2} foi : {self.numero1 + self.numero2}'
     
     def subtracao(self):
-        return f'A subtração foi de : {self.numero1 - self.numero2}'
+        return f'A subtração de {self.numero1} e {self.numero2} foi : {self.numero1 - self.numero2}'
     
     def multiplicacao(self):
-        return f'A multiplicação foi de {self.numero1 * self.numero2}'
+        return f'A multiplicação de {self.numero1} e {self.numero2} foi : {self.numero1 * self.numero2}'
    
     def divisao(self):
-        return f'A divisão doi de: {self.numero1 / self.numero2}'
+        return f'A divisão de {self.numero1} e {self.numero2} foi : {self.numero1 / self.numero2}'
     
     def potencia(self):
-        return f'A potência foi de : {math.pow(self.numero1,self.numero2)}'
+        return f'A potência de ({self.numero1} e {self.numero2}) foi : {math.pow(self.numero1,self.numero2)}'
     
     def resto(self):
-        return f'O resto foi de : {self.numero1 % self.numero2}'
+        return f'O resto  de {self.numero1} + {self.numero2} foi : {self.numero1 % self.numero2}'
    
     def raiz_soma_dois(self):
-        return f'A raiz da soma foi de: {math.sqrt(self.numero1 + self.numero2)}'
+        return f'A raiz da soma  de ({self.numero1} + {self.numero2}) foi : {math.sqrt(self.numero1 + self.numero2)}'
     
         
 def menu(op,a,b):
     while(True):
+        
         calc = Calculadora(a,b)
         print(f'{op}')
-        print(f'thes result is {calc.soma()}')
-        print(f'thes result is {calc.subtracao()}')
-        print(f'thes result is {calc.divisao()}')
-        print(f'thes result is {calc.potencia()}')
-        print(f'thes result is {calc.resto()}')
-        print(f'thes result is {calc.raiz_soma_dois()}')
+        print(calc.soma())
+        print(calc.subtracao())
+        print(calc.divisao())
+        print(calc.subtracao())
+        print(calc.potencia())
+        print(calc.resto())
+        print(calc.raiz_soma_dois())
         return
                 
         
@@ -49,6 +51,10 @@ op = input('Escolha uma das opções: '
            '( % )Resto\n'
            '( sqrt )Raiz\n'
            'Digite a operação desejada: ')
-n1 = int(input("Digite o penultimo digito do seu RU: "))
-n2 = int(input("Digite o último digito do seu RU: "))
-menu(op,n1,n2)
+if not op in '+ - / * ** sqrt %':
+    print('Erro')
+else:
+    n1 = int(input("Digite o penultimo digito do seu RU: "))
+    n2 = int(input("Digite o último digito do seu RU: "))
+    menu(op,n1,n2)
+
