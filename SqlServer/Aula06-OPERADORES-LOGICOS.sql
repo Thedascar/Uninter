c7518c24b5fcb1d4552bc84540be6256be08150e
lucas <lcs_demais@hotmail.com>
2023-07-09 15:20:22 -0300

    add aula 06

---
+++ SqlServer/Aula06-OPERADORES-LOGICOS.sql
@@ -0,0 +1,15 @@
+/*
+
+    Operadores e compração
+    = igual
+    <> diferente de..
+    != diferente de..
+    < menor ou <=
+    > maior
+    ! operador de negação
+    
+*/
+
+SELECT * FROM Person.Person
+    WHERE BusinessEntityID < 10
+
