940a39d8e996148228dd0ce3646b87403da6edb5
lucas <lcs_demais@hotmail.com>
2023-07-12 15:15:38 -0300

    att

---
+++ ProgComp-master/Aula02-INTRODUCAO/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// int main(int argc, char ** argv) chamada para receber informação do S.O.
+int main()
+{
+    using namespace std;
+    cout << "Hello World" << endl;
+    return 0;
+}
