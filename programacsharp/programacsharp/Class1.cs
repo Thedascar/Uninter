61cae4898d96e5f0a496acc70ea0bc4f83641dbc
lucas <lcs_demais@hotmail.com>
2023-09-09 14:12:25 -0300

    aulas c#

---
+++ programacsharp/programacsharp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programacsharp
+{
+    internal class Class1
+    {
+        int a = 10;
+
+        public override string? ToString()
+        {
+            return $"o número da classe é : int -> {a}";
+        }
+    }
+}
---
+++ programacsharp/programacsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using programacsharp;
+
+String nome = "Lucas";
+int idade = 28;
+bool ativo = true;
+
+Console.WriteLine($"Hy my name is {nome} and ive {idade} years old" +
+    $"and {ativo}");
+
+Class1 a = new Class1();
+
+Console.WriteLine(a.ToString());
+
+
+object obj1 = new object();
+var obj2 = new object();
+
+var obj3 = obj2;
+
+Console.WriteLine("Comparando objs");
+Console.WriteLine($"{obj1.GetType().Name}| {obj2.GetType().Name}| {obj3.GetType().Name}");
+Console.WriteLine(obj2==obj3);
+Console.WriteLine(obj2 == obj1);
